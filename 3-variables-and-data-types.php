ae1053934a0766be11779ffa809a1368d00bd66d
Antonio Gutierrez <antonio@phpfullstack.com.br>
2024-12-31 17:37:07 -0300

    Variables and Data Types

---
+++ 3-variables-and-data-types.php
@@ -0,0 +1,12 @@
+<?php
+$name = "John";
+echo "My father's name is " . $name;
+
+$name = "Anthony Richard";
+echo "\nMy brother's name is " . $name;
+
+$myAge = 42;
+echo "\nI am " . $myAge . " years old";
+
+$amIAlive = true;
+echo "\nI am " . ($amIAlive ? "alive" : "dead");
