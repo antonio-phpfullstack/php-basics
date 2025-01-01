3efd366b068a7c364f6b07187855c6ce579384ca
Antonio Gutierrez <antonio@phpfullstack.com.br>
2024-12-31 23:46:21 -0300

    Integrating PHP with HTML

---
+++ 10-integrating-php-with-html.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basic HTML and PHP Code</title>
+</head>
+<body>
+<h1><?php echo "Welcome!"; ?></h1>
+<p><?="This is a basic HTML structure."; ?></p>
+</body>
+</html>
