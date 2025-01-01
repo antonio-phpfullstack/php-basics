c0c335ced15a441429d7c6071f32c473ad31c086
Antonio Gutierrez <antonio@phpfullstack.com.br>
2024-12-31 22:58:26 -0300

    Introduction to Arrays

---
+++ 8-introduction-to-arrays.php
@@ -0,0 +1,19 @@
+<?php
+$array = [1, 2, 3];
+$array[] = 4;
+
+var_dump($array);
+
+$arrayName = array("Anthonya", "Maria", "Juan");
+print_r($arrayName);
+
+echo "Position 0 = {$arrayName[0]}";
+echo "\nPosition 1 = {$arrayName[1]}";
+echo "\nPosition 2 = {$arrayName[2]}";
+
+$countries = ["Argentina",
+    "Brazil",
+    "Chile"
+];
+
+print_r($countries);
