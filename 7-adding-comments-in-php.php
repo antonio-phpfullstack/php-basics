9763dd172bb0ddf243bb1a8ccff309962745e700
Antonio Gutierrez <antonio@phpfullstack.com.br>
2024-12-31 22:32:20 -0300

    Adding Comments in PHP

---
+++ 7-adding-comments-in-php.php
@@ -0,0 +1,24 @@
+<?php
+//One line comment
+/*
+ Multiple
+Line
+Comment
+ */
+
+$sum = 2 + 2; //This is a sum
+/*
+And this is another
+operator
+*/
+$multiple = 2 * 2;
+
+$outputSum = "The result of the sum is $sum";
+$outputMultiple = "The result of the multiplication is $multiple";
+
+echo $outputSum;
+echo "\n{$outputMultiple}";
+
+/*
+ * Documentation
+ * */
