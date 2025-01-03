a4ce2e4ae5fd157b35cfbe73d151981ec77d2753
Antonio Gutierrez <antonio@phpfullstack.com.br>
2025-01-03 15:37:24 -0300

    009-Associative Arrays with Keys and Values

--- 009-associative-arrays-with-keys-and-values.php
+++ 009-associative-arrays-with-keys-and-values.php
@@ -13,3 +13,17 @@ echo "My first name is {$mixArray['firstName']}";
 echo "\nMy second name is {$mixArray['secondName']}";
 echo "\nMy grandfather's age is {$mixArray[0]} years";
 
+$myArray = [
+    "fruits" => ["apple", "banana", "orange"],
+    "vegetables" => ["carrot", "potato", "onion"],
+    "meat" => ["beef", "pork", "chicken"],
+    "dairy" => ["milk", "yogurt", "cheese"],
+    "drinks" => ["water", "tea", "coffee"],
+    "food" => ["pizza", "burger", "salad"],
+    "sports" => ["tennis", "soccer", "basketball"]
+];
+
+echo "My favorite food is {$myArray['food'][0]}\n";
+echo "My favorite sport is {$myArray['sports'][2]}\n";
+echo "My favorite drink is {$myArray['drinks'][0]}\n";
+echo "My favorite fruit is {$myArray['vegetables'][1]}\n";
