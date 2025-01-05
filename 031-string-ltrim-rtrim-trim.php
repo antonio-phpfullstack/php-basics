3ac55f13ee09c76b4c0adde2e935cbd912bc14d3
Antonio Gutierrez <antonio@phpfullstack.com.br>
2025-01-05 17:17:24 -0300

    031-Trimming strings in PHP (ltrim, rtrim, trim)

---
+++ 031-string-ltrim-rtrim-trim.php
@@ -0,0 +1,9 @@
+<?php
+$leftTrim = ltrim("  Esther Maria  ");
+$rightTrim = rtrim("  Esther Maria  ");
+$trim = trim("  Esther Maria  ");
+var_dump($leftTrim);
+echo "\n";
+var_dump($rightTrim);
+echo "\n";
+var_dump($trim);
