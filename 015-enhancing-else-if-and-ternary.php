024dc4ca32324d33acba659647588d9f978b3a6a
Antonio Gutierrez <antonio@phpfullstack.com.br>
2025-01-06 18:54:01 -0300

    015-enhancing-else-if-ternary-switch-match.php

--- 015-enhancing-else-if-and-ternary.php
+++ 015-enhancing-else-if-and-ternary.php
@@ -16,3 +16,48 @@ if (!$failedDueToAbsences) {
 } else {
     echo "The student failed due to absence\n";
 }
+
+$number = 15;
+if ($number === 15) {
+    echo "The number is 15\n";
+} else if ($number === 16) {
+    echo "The number is 16\n";
+} else if ($number === 17) {
+    echo "The number is 17\n";
+} else if ($number === 18) {
+    echo "The number is 18\n";
+} else if ($number === 19) {
+    echo "The number is 19\n";
+} else {
+    echo "The number is not 15, 16, 17, 18 or 19\n";
+}
+
+switch ($number) {
+    case 15:
+        echo "The number is 15\n";
+        break;
+    case 16:
+        echo "The number is 16\n";
+        break;
+    case 17:
+        echo "The number is 17\n";
+        break;
+    case 18:
+        echo "The number is 18\n";
+        break;
+    case 19:
+        echo "The number is 19\n";
+        break;
+    default:
+        echo "The number is not 15, 16, 17, 18 or 19\n";
+        break;
+}
+
+echo match ($number) {
+    15 => "The number is 15\n",
+    16 => "The number is 16\n",
+    17 => "The number is 17\n",
+    18 => "The number is 18\n",
+    19 => "The number is 19\n",
+    default => "The number is not 15, 16, 17, 18 or 19\n",
+};
